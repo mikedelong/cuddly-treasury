537ce0b0e64a11937eee26319631b671459d3791
Mike DeLong <mikedelong@outlook.com>
2022-04-22 15:16:02 -0400

    cleanup: remove incorrect comment

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ ENDPOINT = 'v2/accounting/od/avg_interest_rates'
 FILTER = 'record_date:gte:2015-01-01'
 FIELDS = 'record_date,security_type_desc,security_desc,avg_interest_rate_amt,src_line_nbr,'
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     url = '{}{}?fields={}&filter={}'.format(BASE_URL, ENDPOINT, FIELDS, FILTER)
     print(url)
