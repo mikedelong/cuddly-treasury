c5d9a202cd9dfb7582c9dc261417a5c69dd583c3
Mike DeLong <mikedelong@outlook.com>
2022-04-23 15:39:42 -0400

    feature: introduce logging

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from pandas import DataFrame
 from requests import get
+from logging import INFO
+from logging import basicConfig
+from logging import getLogger
+from arrow import now
 
 BASE_URL = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/'
 ENDPOINT = 'v2/accounting/od/avg_interest_rates'
@@ -7,13 +11,17 @@ FILTER = 'record_date:gte:2015-01-01'
 FIELDS = 'record_date,security_type_desc,security_desc,avg_interest_rate_amt,src_line_nbr,'
 
 if __name__ == '__main__':
+    TIME_START = now()
+    LOGGER = getLogger(__name__, )
+    basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
+    LOGGER.info('started')
+
     url = '{}{}?fields={}&filter={}'.format(BASE_URL, ENDPOINT, FIELDS, FILTER)
-    print(url)
+    LOGGER.info('url: %s', url)
     response = get(url=url)
     data = response.json()['data']
-    print(type(data))
-    print(len(data))
+    LOGGER.info('count: %d', len(data))
     data_df = DataFrame.from_records(data=data, index=None, exclude=None)
-    print(data_df.shape)
+    LOGGER.info('shape: %s', data_df.shape)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
