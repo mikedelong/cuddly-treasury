86a3180e17cf236c9e9959d1fb9a612d26523055
Mike DeLong <mikedelong@outlook.com>
2022-04-27 11:59:02 -0400

    feature: switch to downloading CSV and simplify the fetch

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ from logging import basicConfig
 from logging import getLogger
 
 from arrow import now
-from pandas import DataFrame
-from requests import get
+from pandas import read_csv
 
 BASE_URL = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/'
 ENDPOINT = 'v2/accounting/od/avg_interest_rates'
@@ -17,12 +16,9 @@ if __name__ == '__main__':
     basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
     LOGGER.info('started')
 
-    url = '{}{}?fields={}&filter={}'.format(BASE_URL, ENDPOINT, FIELDS, FILTER)
+    url = '{}{}?fields={}&filter={}&format=csv'.format(BASE_URL, ENDPOINT, FIELDS, FILTER)
     LOGGER.info('url: %s', url)
-    response = get(url=url)
-    data = response.json()['data']
-    LOGGER.info('count: %d', len(data))
-    data_df = DataFrame.from_records(data=data, index=None, exclude=None)
+    data_df = read_csv(filepath_or_buffer=url)
     LOGGER.info('shape: %s', data_df.shape)
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
