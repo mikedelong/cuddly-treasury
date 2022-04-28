4ed6468ecdefb9df34b1f572a36ab48a55edb390
Mike DeLong <mikedelong@outlook.com>
2022-04-28 10:36:37 -0400

    feature: a first cut at plotting the various rate series; still kind of a mess

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logging import getLogger
 
 from arrow import now
 from pandas import read_csv
+from seaborn import catplot
+from matplotlib.pyplot import show
 
 BASE_URL = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/'
 ENDPOINT = 'v2/accounting/od/avg_interest_rates'
@@ -31,5 +33,10 @@ if __name__ == '__main__':
     for security_type in security_types:
         LOGGER.info('%s : %d', security_type, len(data_df[data_df[SECURITY] == security_type]))
 
-    LOGGER.info('start date: %s', data_df['record_date'].min())
+    columns = ['record_date', 'security_desc', 'avg_interest_rate_amt']
+    melted_df = data_df[columns].melt(id_vars=['record_date', 'security_desc'],
+                                      value_vars=['avg_interest_rate_amt']).drop(columns=['variable'])
+    catplot(data=melted_df, hue='security_desc', kind='point', x='record_date', y='value')
+    show()
+
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
