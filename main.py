376d84f5600dd69d27849edd89e0e429895ae912
Mike DeLong <mikedelong@outlook.com>
2022-04-29 10:13:42 -0400

    refactor: fold in a local variable in the DataFrame cleanup code

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from logging import INFO
 from logging import basicConfig
 from logging import getLogger
 
+import plotly.express as px
 from arrow import now
 from pandas import read_csv
-
-import plotly.express as px
 
 BASE_URL = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/'
 ENDPOINT = 'v2/accounting/od/avg_interest_rates'
@@ -27,19 +26,18 @@ if __name__ == '__main__':
     LOGGER.info('url: %s', url)
     data_df = read_csv(filepath_or_buffer=url)
     LOGGER.info('shape: %s', data_df.shape)
-
     LOGGER.info('data headings: %s', list(data_df))
-    security_types = sorted(data_df[SECURITY].unique())
-    for security_type in security_types:
-        LOGGER.info('%s : %d', security_type, len(data_df[data_df[SECURITY] == security_type]))
 
-    # remove the series we don't care about
-    remove = ['Hope Bonds', 'R.E.A. Series', 'Treasury Inflation-Indexed Bonds',
-              'Treasury Inflation-Indexed Notes']
-    data_df = data_df[~data_df['security_desc'].isin(remove)]
+    # remove the series we don't care about because they've been discontinued
+    data_df = data_df[~data_df['security_desc'].isin(['Hope Bonds', 'R.E.A. Series',
+                                                      'Treasury Inflation-Indexed Bonds',
+                                                      'Treasury Inflation-Indexed Notes'])]
     # fix one weird datapoint
     data_df['security_desc'] = data_df['security_desc'].apply(
         lambda x: x if x != 'TotalMarketable' else 'Total Marketable')
+
+    for security_type in sorted(data_df[SECURITY].unique()):
+        LOGGER.info('%s : %d', security_type, len(data_df[data_df[SECURITY] == security_type]))
 
     use_columns = ['record_date', 'security_desc', 'avg_interest_rate_amt']
     map_columns = {'record_date': 'date', 'avg_interest_rate_amt': 'rate'}
