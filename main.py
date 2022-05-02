d9aa884f4faca56195a0822f20da3a3cc8bcf72d
Mike DeLong <mikedelong@outlook.com>
2022-05-02 13:12:12 -0400

    refactor: centralize the definition of the filter date

--- main.py
+++ main.py
@@ -9,20 +9,23 @@ from pandas import read_csv
 
 BASE_URL = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/'
 COLUMNS = {'record_date': 'date', 'avg_interest_rate_amt': 'rate'}
+DAY = 1
 ENDPOINT = 'v2/accounting/od/avg_interest_rates'
 FIELDS = 'record_date,security_desc,avg_interest_rate_amt,'
-FILTER = 'record_date:gte:2001-01-01'
 FORMAT = 'csv'
+MONTH = 1
 SECURITY = 'security_desc'
+YEAR = 2001
 
 if __name__ == '__main__':
     TIME_START = now()
-    size = 17 * (TIME_START.date() - datetime.date(year=2001, month=1, day=1)).days // 30
+    size = 17 * (TIME_START.date() - datetime.date(year=YEAR, month=MONTH, day=DAY)).days // 30
     LOGGER = getLogger(__name__, )
     basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
     LOGGER.info('started')
 
-    url = '{}{}?fields={}&filter={}&format={}&page[size]={}'.format(BASE_URL, ENDPOINT, FIELDS, FILTER,
+    our_filter = 'record_date:gte:{}-{:02d}-{:02d}'.format(YEAR, MONTH, DAY)
+    url = '{}{}?fields={}&filter={}&format={}&page[size]={}'.format(BASE_URL, ENDPOINT, FIELDS, our_filter,
                                                                     FORMAT, size)
     LOGGER.info('url: %s', url)
     data_df = read_csv(filepath_or_buffer=url)
