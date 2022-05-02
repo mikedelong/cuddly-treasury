7e20cbd17e0ec04b25da7b682c37bdd2c9c4011f
Mike DeLong <mikedelong@outlook.com>
2022-05-02 12:44:00 -0400

    feature: build the source URL and get the data

--- debt.py
+++ debt.py
@@ -4,12 +4,30 @@ from logging import basicConfig
 from logging import getLogger
 
 from arrow import now
+from pandas import read_csv
+
+BASE_URL = 'https://api.fiscaldata.treasury.gov/services/api/fiscal_service/'
+DAY = 31
+ENDPOINT = 'v2/accounting/od/debt_to_penny'
+FIELDS = 'record_date,debt_held_public_amt,intragov_hold_amt,tot_pub_debt_out_amt,'
+FORMAT = 'csv'
+MONTH = 3
+YEAR = 2005
 
 if __name__ == '__main__':
     TIME_START = now()
-    size = 17 * (TIME_START.date() - datetime.date(year=2001, month=1, day=1)).days // 30
+    our_filter = 'record_date:gte:{}-{:02d}-{:02d}'.format(YEAR, MONTH, DAY)
+    size = 1 * (TIME_START.date() - datetime.date(year=YEAR, month=MONTH, day=DAY)).days
     LOGGER = getLogger(__name__, )
     basicConfig(format='%(asctime)s : %(name)s : %(levelname)s : %(message)s', level=INFO, )
     LOGGER.info('started')
+
+    url = '{}{}?fields={}&filter={}&format={}&page[size]={}'.format(BASE_URL, ENDPOINT, FIELDS, our_filter,
+                                                                    FORMAT, size)
+
+    LOGGER.info('url: %s', url)
+    data_df = read_csv(filepath_or_buffer=url)
+    LOGGER.info('shape: %s', data_df.shape)
+    LOGGER.info('data headings: %s', list(data_df))
 
     LOGGER.info('total time: {:5.2f}s'.format((now() - TIME_START).total_seconds()))
